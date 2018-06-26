0d879fd5cfcaf61f5041b914832a5dbf4c9080de
SO4P <471804582@qq.com>
2018-06-26 18:11:49 +0800

    Assets

---
+++ Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Complete
+{
+    public class UI : NetworkBehaviour
+    {
+
+        TankHealth hp;
+
+        // Use this for initialization
+        void Start()
+        {
+            hp = this.GetComponent<TankHealth>();
+        }
+
+        void OnGUI()
+        {
+            if (!isLocalPlayer)
+                return;
+            string str = "current hp:";
+            str += hp.getHp();
+            GUI.TextField(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 100, 100, 100), str);
+        }
+    }
+}
